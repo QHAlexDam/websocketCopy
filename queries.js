512482d8a9c97ea9cbf43daa5f91d9f3b7041c6d
Alexandre Dam <44511455+QHAlexDam@users.noreply.github.com>
2021-02-19 12:07:21 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   // database: 'projet 3',
   // password: 'postgrespw',
 
-  host: 'exotik-db.postgres.database.azure.com',
+  host: 'testwebsocketserver1.azurewebsites.net',
   database: "postgres",
   user: 'admin_exotik@exotik-db',
   password: 'T79?2m>(CkdMqC5N',
